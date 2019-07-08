d4556f182cc8cbadc420bcda1dfe9ddbd8e1bee2
Perehodko <n.perehodko@yandex.ru>
2019-07-08 21:24:08 +0300

    add task 2.3 step 5

---
+++ 2.3lesson_step5.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+import math
+
+
+link = "http://suninjuly.github.io/redirect_accept.html"
+
+driver = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
+driver.get(link)
+
+
+button = driver.find_element_by_class_name("trollface.btn.btn-primary")
+
+button.click()
+time.sleep(1)
+
+new_window = driver.window_handles[1]
+driver.switch_to.window(new_window)
+time.sleep(1)
+
+x_el = driver.find_element_by_id('input_value')
+
+x = int(x_el.text)
+#print(x)
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+y = calc(x)
+
+textarea = driver.find_element_by_id("answer")
+textarea.send_keys(y)
+time.sleep(1)
+
+button = driver.find_element_by_class_name("btn.btn-primary")
+
+button.click()
+
