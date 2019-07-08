63595ef1deb13add1d12ce06808a2a5b66634e0d
Perehodko <n.perehodko@yandex.ru>
2019-07-08 21:01:24 +0300

    add tast 2.1 step 7

---
+++ 2.1lesson_step7.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+import math
+
+
+link = "http://suninjuly.github.io/get_attribute.html"
+
+driver = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
+driver.get(link)
+
+
+
+img = driver.find_element_by_id('treasure')
+
+pic_attribute = img.get_attribute("valuex")
+x = int(pic_attribute)
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+y = calc(x)
+
+textarea = driver.find_element_by_id("answer")
+textarea.send_keys(y)
+time.sleep(3)
+
+check_box = driver.find_element_by_id("robotCheckbox")
+check_box.click()
+
+radiobutton = driver.find_element_by_id("robotsRule")
+radiobutton.click()
+
+button = driver.find_element_by_css_selector("button.btn.btn-default")
+
+button.click()
+
